56849b81bc71b22d7fcf018c2f8d5349aff304c9
hortensemasurel <hortense.masurel@gmail.com>
2019-11-21 16:44:07 +0100

    déplacement des pions même si problème sur le pion qui bouge !

--- interface_constantes.py
+++ interface_constantes.py
@@ -6,6 +6,7 @@ from pygame.locals import *
 number_square_side = 13
 square_dimension = 60
 window_dimension = number_square_side*square_dimension
+bandeau = 20
 
 #titre et icone de la fenêtre
 icon_image = 'images/maison.png'
@@ -15,7 +16,114 @@ title = 'MonoPOOAly'
 #liste des images pour le jeu
 background_homepage = 'images/fond.jpg'
 background_play = 'images/fond.jpg'
-house_image = 'images/maison.png'
-hotel_image ='images/hotel.png'
-player_dog = 'images/chien.png'
-player_car = 'imaes/car.png'
+houses_1= 'images/1_houses.png'
+houses_2= 'images/2_houses.png'
+houses_3= 'images/3_houses.png'
+houses_4= 'images/4_houses.png'
+houses_5= 'images/5_houses.png'
+piece_1= 'images/piece_1.png'
+piece_2= 'images/piece_2.png'
+piece_3= 'images/piece_3.png'
+
+# on veut une liste pour chaque case i avec
+# [x_maison, y_maison, x_pion1, y_pion1, x_pion2, y_pion2, x_pion3, y_pion3, x_pion4, y_pion4]
+coordonnees = [[0]]*41
+
+
+for i in range(0, 11):  # ligne du bas en partant de la case départ
+    #pour les maisons
+    x_maison = (number_square_side - 2 - i) * square_dimension  # On compte à partir de 0
+    y_maison = 11 * square_dimension  # dernière ligne
+
+    #pour le pion 1 (haut gauche)
+    x_pion1=(number_square_side - 2 - i) * square_dimension
+    y_pion1=11 * square_dimension+ bandeau
+
+    # pour le pion 2 (haut droit)
+    x_pion2 = (number_square_side - 2 - i) * square_dimension + square_dimension/2
+    y_pion2 = 11 * square_dimension + bandeau
+
+    # pour le pion 3 (bas gauche)
+    x_pion3 = (number_square_side - 2 - i) * square_dimension
+    y_pion3 = 11 * square_dimension + bandeau + (2*square_dimension - bandeau)/2
+
+    # pour le pion 4 (bas droit)
+    x_pion4 = (number_square_side - 2 - i) * square_dimension + square_dimension/2
+    y_pion4 = 11 * square_dimension + bandeau + (2*square_dimension - bandeau)/2
+
+    coordonnees[i] = [x_maison, y_maison, x_pion1, y_pion1, x_pion2, y_pion2, x_pion3, y_pion3, x_pion4, y_pion4]
+
+for i in range(11, 21):  # première colonne de la prison au parc gratuit
+    # pour les maisons
+    x_maison = 0 * square_dimension + (2*square_dimension-bandeau)
+    y_maison = (number_square_side + 8 - i) * square_dimension
+
+    # pour le pion 1 (haut gauche)
+    x_pion1 = 0 * square_dimension
+    y_pion1 = (number_square_side + 8 - i) * square_dimension
+
+    # pour le pion 2 (haut droit)
+    x_pion2 = 0 * square_dimension + (2*square_dimension-bandeau)/2
+    y_pion2 = (number_square_side + 8 - i) * square_dimension
+
+    # pour le pion 3 (bas gauche)
+    x_pion3 = 0 * square_dimension
+    y_pion3 = (number_square_side + 8 - i) * square_dimension + square_dimension/2
+
+    # pour le pion 4 (bas droit)
+    x_pion4 = 0 * square_dimension + (2*square_dimension-bandeau)/2
+    y_pion4 = (number_square_side + 8 - i) * square_dimension + square_dimension/2
+
+    coordonnees[i] = [x_maison, y_maison, x_pion1, y_pion1, x_pion2, y_pion2, x_pion3, y_pion3, x_pion4, y_pion4]
+
+for i in range(21, 31):  # première ligne du parc gratuit à la case 'allez en prison'
+    # pour les maisons
+    x_maison = (i - 19) * square_dimension
+    y_maison = 0 * square_dimension + (2*square_dimension-bandeau)
+
+    # pour le pion 1 (haut gauche)
+    x_pion1 = (i - 19) * square_dimension
+    y_pion1 = 0
+
+    # pour le pion 2 (haut droit)
+    x_pion2 = (i - 19) * square_dimension + square_dimension/2
+    y_pion2 = 0
+
+    # pour le pion 3 (bas gauche)
+    x_pion3 = (i - 19) * square_dimension
+    y_pion3 = 0 + (2*square_dimension-bandeau)/2
+
+    # pour le pion 4 (bas droit)
+    x_pion4 = (i - 19) * square_dimension + square_dimension/2
+    y_pion4 = 0 + (2*square_dimension-bandeau)/2
+
+    coordonnees[i] = [x_maison, y_maison, x_pion1, y_pion1, x_pion2, y_pion2, x_pion3, y_pion3, x_pion4, y_pion4]
+
+for i in range(31, 41):  # dernière colonne de 'allez en prison' à la case départ
+    # pour les maisons
+    x_maison = 11 * square_dimension
+    y_maison = (i - 29) * square_dimension
+
+    # pour le pion 1 (haut gauche)
+    x_pion1 = 11 * square_dimension + bandeau
+    y_pion1 = (i - 29) * square_dimension
+
+    # pour le pion 2 (haut droit)
+    x_pion2 = 11 * square_dimension + bandeau + (2*square_dimension-bandeau)/2
+    y_pion2 = (i - 29) * square_dimension
+
+    # pour le pion 3 (bas gauche)
+    x_pion3 = 11 * square_dimension + bandeau
+    y_pion3 = (i - 29) * square_dimension + square_dimension/2
+
+    # pour le pion 4 (bas droit)
+    x_pion4 = 11 * square_dimension + bandeau + (2*square_dimension-bandeau)/2
+    y_pion4 = (i - 29) * square_dimension + square_dimension/2
+
+    coordonnees[i] = [x_maison, y_maison, x_pion1, y_pion1, x_pion2, y_pion2, x_pion3, y_pion3, x_pion4, y_pion4]
+
+
+
+
+
+
--- pygame_simple.py
+++ pygame_simple.py
@@ -5,6 +5,9 @@ import json
 import board
 import time
 
+global piece_2, piece_3
+global houses_1,houses_2,houses_3,houses_4,houses_5
+
 
 pygame.init()
 #
@@ -40,3 +43,46 @@ def choose_actions_pygame():
                 choice = liste.index(nom)
                 return choice
     return -1
+
+def blit_piece(surface,position, piece, indice_x, indice_y):
+    """
+    La position est la place de la case sur le plateau.
+    Piece est le pion à coller à cette position.
+    :param surface:
+    :param position:
+    :return:
+    """
+
+    pion = pygame.image.load('images/'+piece+'.png').convert_alpha()
+    surface.blit(pion, (coordonnees[position][indice_x], coordonnees[position][indice_y]))
+
+def blit_houses(surface,position, houses):
+    """
+    La position est la place de la case sur le plateau.
+    Houses est le groupe de maison à coller à cette position.
+    :param surface:
+    :param position:
+    :return:
+    """
+    maison = pygame.image.load(houses).convert_alpha()
+    surface.blit(maison, (coordonnees[position][0], coordonnees[position][1]))
+
+def blit_images_on_board(surface,dict_board_state):
+    """
+    on regarde chaque case du plateau et on va mettre les images de maisons et de pions nécessaires
+    :param dict_board_state:
+    :return:
+    """
+    for position in list(dict_board_state.keys())[1:]:
+        #on colle le groupe de maison approprié
+
+        nb_houses = dict_board_state[position]["nb_houses"]
+        if nb_houses>0:
+            image_house = 'houses_'+str(nb_houses)
+            blit_houses(surface,int(position),image_house)
+
+        #on colle les pions
+        if len(dict_board_state[position]["players"])>0:
+            for i in range(len(dict_board_state[position]["players"])):
+                image_piece = 'piece_'+str(i+1)
+                blit_piece(surface, int(position), image_piece,2*(i+1),2*(i+1)+1)
