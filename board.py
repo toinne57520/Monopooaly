4383cd206b1bb2efa7c80c698a61bdad9107e68a
toinne57520 <a.bour1210@gmail.com>
2019-11-23 14:14:07 +0100

    correction serialize_board

--- board.py
+++ board.py
@@ -56,7 +56,7 @@ class Board :
 
         if player.in_dept :
             player.in_dept = False
-            early_action = player.choose_actions(self.square_list[player.position].get_actions(player))
+            early_action = (self.square_list[player.position].get_actions(player))
         else :
             early_action = {0: 'Lancer les dés'}
             i = 0
